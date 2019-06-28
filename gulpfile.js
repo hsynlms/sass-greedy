e6442a608d49be47ed6658a858ea582ea0f1a73b
Huseyin Elmas <hsynlms47@gmail.com>
2019-06-28 21:07:15 +0300

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,50 @@
+const chalk = require('chalk');
+const gulp = require('gulp');
+const del = require('del');
+const concat = require('gulp-concat');
+const header = require('gulp-header');
+const pkg = require('./package.json');
+
+const tpl = '/*!\n* sass-greedy v<%= version %>\n* A flexible, lightweight and simple grid generator for sass.\n*\n* Author: <%= author %>\n*/\n';
+
+const paths = {
+  sass: [
+    './src/helpers/*.scss',
+    './src/generators/*.scss',
+    './src/*.scss'
+  ],
+  dist: './dist/'
+};
+
+// build greedy
+gulp.task('build', function () {
+  return gulp.src(paths.sass)
+  .pipe(concat('_greedy.scss'))
+    .pipe(
+      header(
+        tpl,
+        {
+          version: pkg.version,
+          author: pkg.author
+        }
+      )
+    )
+    .pipe(gulp.dest(paths.dist))
+    .on('end', function () {
+      console.log(chalk.green('Build process has been completed successfully.'));
+    });
+});
+
+// clean dist folder
+gulp.task('clean', function () {
+  return del(paths.dist);
+});
+
+// default task
+gulp.task(
+  'default',
+  gulp.series(
+    'clean',
+    'build'
+  )
+);
